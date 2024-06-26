40b7419ebc9267adf60c9587d487c32ccb21a576
BuildAndDestroy <devreap1@gmail.com>
2024-06-26 14:54:32 -0600

    cryptominers: Monero and derohe
    Add cryptominers and update python program
    Update fileserver to match new capabilities

--- fileserver/fileserver/tearmeup.py
+++ fileserver/fileserver/tearmeup.py
@@ -5,21 +5,37 @@ Chop up a base64 file into smaller than 1M files. This is needed
 due to a limitation with powershell empire's limit of 1M files.
 """
 
+import argparse
 import glob
 
-SEEK_FILE = 'xmrig.b64'
+# SEEK_FILE = 'xmrig.b64'
 
 
-def file_check() -> None:
+def parse_arguments() -> tuple:
+    """Obtain user input and parse"""
+    user_license = 'Copyright (C) 2017  Mitch O\'Donnell\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions.'
+    url_repo = 'https://github.com/BuildAndDestroy/powershell-empire-custom'
+    epilog = '{}\n\n{}'.format(user_license, url_repo)
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument(
+        'file_name', help='File you wish to shred into 1M files.\n    [*] Example: xmrig.b64')
+    args = parser.parse_args()
+
+    return args
+
+
+def file_check(file_name) -> None:
     """Check for our base64 file and throw exception if not found"""
-    check_for_file = glob.glob(SEEK_FILE)
-    if not check_for_file[0]:
-        raise ValueError(f'File {SEEK_FILE} does not exist!')
+    check_for_file = glob.glob(file_name)
+    if not check_for_file:
+        raise ValueError(f'File {file_name} does not exist!')
 
 
-def read_b64_file() -> str:
+def read_b64_file(file_name) -> str:
     """Read the file that is base64 encoded"""
-    with open(SEEK_FILE, 'r') as b64_file:
+    with open(file_name, 'r') as b64_file:
         b64_string = b64_file.read()
     return b64_string
 
@@ -38,8 +54,9 @@ def chunk_files(b64_string) -> None:
 def main() -> None:
     """Execute the program"""
 
-    file_check()
-    b64_string = read_b64_file()
+    args = parse_arguments()
+    file_check(args.file_name)
+    b64_string = read_b64_file(args.file_name)
     chunk_files(b64_string)
 
 
